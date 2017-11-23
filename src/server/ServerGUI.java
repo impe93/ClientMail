6b2f07d03fffa353fdf58111a64fddcdabbd77fc
Alessio Berger <berger.alessio@educ.di.unito.it>
2017-11-23 15:00:26 +0100

    Modifiche a: ServerGUI
    * terminata l'interfaccia grafica del server, inserito un BorderLayout
      con una label, un scrollpanel centrale per il mantenimento dei log e 
      un pulsante a fondo pagina per chiudere il server.

--- src/server/ServerGUI.java
+++ src/server/ServerGUI.java
@@ -1,9 +1,18 @@
 package server;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 /**
  *
@@ -14,13 +23,37 @@ public class ServerGUI {
     public ServerGUI(){
     
         JFrame frame = new JFrame("Server");
+        
         JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(800, 200));
+        panel.setLayout(new BorderLayout());
+        panel.setPreferredSize(new Dimension(800, 400));
+        Border bordoTrenta = BorderFactory.createEmptyBorder(30, 30, 30, 30);
+        panel.setBorder(bordoTrenta);
         frame.add(panel);
+     
+        JLabel lblIntestazione = new JLabel("Log delle operazioni:");
+        Border bordoDieci = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+        lblIntestazione.setBorder(bordoDieci);
+        panel.add(lblIntestazione, BorderLayout.PAGE_START);
+        lblIntestazione.setHorizontalAlignment( JLabel.CENTER );
+        
+        JTextArea log = new JTextArea();
+        log.setLineWrap(true);
+        log.setBorder(bordoDieci);
+        JScrollPane sp = new JScrollPane(log);
+        
+        panel.add(sp, BorderLayout.CENTER);
+        
+        JPanel footer = new JPanel();
+        footer.setBorder(bordoDieci);
+        JButton chiudi = new JButton("Chiudi il server");
+        footer.add(chiudi);
+        frame.add(footer, BorderLayout.PAGE_END);
+        
+        
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        
         
     }
     
