552015472c9481f9ea7226920653f7f05373ba52
Alessio Berger <berger.alessio@educ.di.unito.it>
2017-11-23 11:21:35 +0100

    Modifiche a: Email, Utente, CasellaPostaElettronica
    * creata classe Email
    * creata classe Utente
    * creata interface CasellaPostaElettronica

---
+++ src/modelli/CasellaPostaElettronica.java
@@ -0,0 +1,9 @@
+package modelli;
+
+/**
+ *
+ * @author Alessio Berger, Lorenzo Imperatrice, Francesca Riddone
+ */
+public interface CasellaPostaElettronica {
+    
+}
---
+++ src/modelli/Email.java
@@ -0,0 +1,112 @@
+package modelli;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Alessio Berger, Lorenzo Imperatrice, Francesca Riddone
+ */
+public class Email {
+    int id;
+    Utente mittente;
+    ArrayList<Utente> destinatari;
+    String oggetto;
+    String corpo;
+    Date data;
+    int priorita;
+    boolean letto;
+    
+   
+    public Email(int id, Utente mittente, ArrayList destinatari, String oggetto, String corpo){
+        this.id = id;
+        this.mittente = mittente;
+        this.destinatari = destinatari;
+        this.oggetto = oggetto;
+        this.corpo = corpo;
+        this.data = new Date();
+        this.priorita = 3;
+        this.letto = false;
+                
+    }
+    
+    public Email(){
+        this.id = 0;
+        this.mittente = null;
+        this.destinatari = null;
+        this.oggetto = null;
+        this.corpo = null;
+        this.data = null;
+        this.priorita = 0;
+        this.letto = false;
+                
+    }
+    
+    /*GETTER*/
+    
+    int getId(){
+        return this.id;
+    }
+    
+    Utente getMittente(){
+        return this.mittente;
+    }
+    
+    ArrayList<Utente> getDestinatari(){
+        return this.destinatari;
+    }
+    
+    String getOggetto(){
+        return this.oggetto;
+    }
+    
+    String getCorpo(){
+        return this.corpo;
+    }
+    
+    Date getData(){
+        return this.data;
+    }
+    
+    int getPriorita(){
+        return this.priorita;
+    }
+    
+    boolean getLetto(){
+        return this.letto;
+    }
+    
+    /*SETTER*/
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMittente(Utente mittente) {
+        this.mittente = mittente;
+    }
+
+    public void setDestinatari(ArrayList<Utente> destinatari) {
+        this.destinatari = destinatari;
+    }
+
+    public void setOggetto(String oggetto) {
+        this.oggetto = oggetto;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public void setPriorita(int priorita) {
+        this.priorita = priorita;
+    }
+
+    public void setLetto(boolean letto) {
+        this.letto = letto;
+    }
+}
