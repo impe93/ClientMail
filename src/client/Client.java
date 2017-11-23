01c88cfcc2702807fa14367971a052c7d66365df
Lorenzo Imperatrice <imperatrice.lorenzo@educ.di.unito.it>
2017-11-23 11:31:26 +0100

    Modifiche Client, ClientGUI, ClientImplementation:
    * Client: Inizializzato
    
    * ClientGUI: Inizializzato
    
    * ClientImplementation: Inizializzato

---
+++ src/client/Client.java
@@ -0,0 +1,17 @@
+package client;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lorenzo Imperatrice, Francesca Riddone, Alessio Berger
+ */
+public interface Client {
+    
+    public boolean inoltraEmail(Email emailDaInoltrare);
+    
+    public ArrayList<Email> ordinaPerPriorita();
+    
+    public 
+    
+}
---
+++ src/client/ClientImplementation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lorenzo Imperatrice, Francesca Riddone, Alessio Berger
+ */
+public class ClientImplementation {
+    
+    private ArrayList<Email> emailRicevute;
+    private ArrayList<Email> emailInviate;
+    
+    
+    
+}
