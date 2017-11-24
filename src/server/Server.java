07ad2d7f8694c37531adf521a121689fc8f706b3
Alessio Berger <berger.alessio@educ.di.unito.it>
2017-11-24 10:37:27 +0100

    Modifiche a: Server, ServerImplementation
    * Reso Server un oggetto RMI
    * effettuata prima implementazione del Server in ServerImplementation

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,14 +3,16 @@ package server;
 import java.util.ArrayList;
 import modelli.Email;
 import modelli.Utente;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  *
  * @author Alessio Berger, Lorenzo Imperatrice, Francesca Riddone
  */
-public interface Server {
-    ArrayList<Email> getInviate(int ultimaInviata, Utente utente);
-    ArrayList<Email> getRicevute(int ultimaRicevuta, Utente utente);
-    boolean eliminaEmail(Email emailDaEliminare, Utente utente);
-    boolean inviaEmail(Email emailDaInviare);
+public interface Server extends Remote{
+    ArrayList<Email> getInviate(int ultimaInviata, Utente utente) throws RemoteException;
+    ArrayList<Email> getRicevute(int ultimaRicevuta, Utente utente) throws RemoteException;
+    boolean eliminaEmail(Email emailDaEliminare, Utente utente) throws RemoteException;
+    boolean inviaEmail(Email emailDaInviare) throws RemoteException;
 }
--- src/server/ServerImplementation.java
+++ src/server/ServerImplementation.java
@@ -1,9 +1,67 @@
 package server;
 
+import client.ClientImplementation;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelli.Email;
+import modelli.Utente;
+
 /**
  *
  * @author Alessio Berger, Lorenzo Imperatrice, Francesca Riddone
  */
-public class ServerImplementation {
+public class ServerImplementation extends UnicastRemoteObject implements Server{
+    
+    public ServerImplementation() throws RemoteException{
+        /* 
+        registrazione dell'oggetto ClientImplementation presso il registro di 
+        bootstrap (rmiregistry)
+        */
+        lanciaRMIRegistry();
+        try {
+            Naming.rebind("//localhost/Server", this);
+            //chiamare metodo di server perchè lui mi registri come oggetto remoto
+        }
+        catch(MalformedURLException | RemoteException e) {
+            Logger.getLogger(ClientImplementation.class.getName()).log(Level.SEVERE, null, e);
+        }
+    
+    }
+
+    @Override
+    public ArrayList<Email> getInviate(int ultimaInviata, Utente utente) throws RemoteException {
+    return null;
+    }
+
+    @Override
+    public ArrayList<Email> getRicevute(int ultimaRicevuta, Utente utente) throws RemoteException {
+    return null;
+    }
+
+    @Override
+    public boolean eliminaEmail(Email emailDaEliminare, Utente utente) throws RemoteException {
+    return false;
+    }
+
+    @Override
+    public boolean inviaEmail(Email emailDaInviare) throws RemoteException {
+    return false;
+    }
+    
+    public static void lanciaRMIRegistry() {
+        try {
+            LocateRegistry.createRegistry(1099);
+            System.out.println("java RMI registry creato");
+        } catch (RemoteException e) {
+            //non fa niente: RMI registry esiste già
+            System.out.println("java RMI registry già esistente!");
+        }
+    }
     
 }
